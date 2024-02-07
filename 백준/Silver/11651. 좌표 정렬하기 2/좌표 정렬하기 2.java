606e3989cd5d0443b6ce1a816e7655b7f3d2f9dc
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-02-07 15:32:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 764 ms, Memory: 53236 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int num = Integer.parseInt(br.readLine());
+        
+        int xy[][] = new int[num][2];
+        
+        for(int i=0; i<num; i++)
+        {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int x = Integer.parseInt(st.nextToken());
+        	int y = Integer.parseInt(st.nextToken());
+        	
+        	xy[i][0] = x;
+        	xy[i][1] = y;
+        }
+        
+      Arrays.sort(xy, new Comparator<int[]>() {
+    	  @Override
+    	  public int compare(int[] o1, int[] o2) {
+    		  if(o1[1] == o2[1])
+    			  //첫 번째 기준으로 오름차순
+    			  return o1[0] -o2[0];
+    		  else
+    			  //두 번째 기준으로 오름차순
+    			  return o1[1]-o2[1];
+    	  }
+      });
+       
+      for(int[] i : xy) {
+    	  bw.write(i[0]+" "+i[1]);
+    	  bw.write("\n");
+      }
+      
+      bw.flush();
+      bw.close();
+        
+    }
+}
