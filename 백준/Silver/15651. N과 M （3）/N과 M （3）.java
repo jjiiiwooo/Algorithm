cfa32ef1764dbfa9b108acd5a1c6e4240380076d
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-02-21 14:44:03 +0900

    [Silver III] Title: N과 M (3), Time: 696 ms, Memory: 211000 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] arr;
+    public static int n,m;
+    public static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        arr = new int[m];
+        
+        dfs(0);
+        System.out.print(sb);
+    }
+    
+    public static void dfs(int depth)
+    {
+        if(depth == m)
+        {
+            for(int i=0; i<m; i++)
+            {
+                sb.append(arr[i]+" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        
+        for(int i=1; i<=n; i++)
+        {
+            arr[depth] = i;
+            dfs(depth+1);
+        }
+    }
+}
