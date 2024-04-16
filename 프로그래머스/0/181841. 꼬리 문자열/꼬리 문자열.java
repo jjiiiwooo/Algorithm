60427918d9165a3ab1dad9d1f6e97fd846d8435b
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-04-16 19:04:05 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.10 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuffer answer = new StringBuffer();
+        
+        for(int i=0; i<str_list.length; i++)
+        {
+            if(!str_list[i].contains(ex))
+            {
+                answer.append(str_list[i]);
+            }
+            
+        }
+        
+        return answer.toString();
+    }
+}
