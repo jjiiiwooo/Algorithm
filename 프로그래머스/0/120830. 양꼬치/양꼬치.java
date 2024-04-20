1adf96fff8730633abfee53617961e247f1efa98
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-04-20 14:23:09 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int free = n/10;
+        
+        answer = n*12000 + k*2000 - free*2000;
+        return answer;
+    }
+}
