8b6ffdca459a10416bd9f0077b5880487013f2c0
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-05-01 17:58:40 +0900

    [D1] Title: 대각선 출력하기, Time: 133 ms, Memory: 26,184 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{	
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<5; i++)
+		{
+			for(int j=0; j<5; j++)
+			{
+				if(i==j)
+				{
+					System.out.print("#");
+				}
+				else
+				{
+					System.out.print("+");
+				}
+			}
+			System.out.println();
+			
+		}
+	}
+}
