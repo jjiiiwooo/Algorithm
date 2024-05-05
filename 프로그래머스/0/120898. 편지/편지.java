5e547b8a0c990fe6e5cf7cc0df4a50312711b4c4
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-05-05 13:43:10 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int len = message.length();
+        answer = len*2;
+        
+        return answer;
+    }
+}
