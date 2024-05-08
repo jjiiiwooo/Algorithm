8e2011d3191b3c75248f764bf092b82bebfd689c
jjiiiwooo <150234478+jjiiiwooo@users.noreply.github.com>
2024-05-08 22:05:23 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 291 ms, Memory: 53,464 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,33 +1,53 @@
 import java.util.*;
-
+ 
 class Solution {    
-    
+     
     public static void main(String args[]) throws Exception {
-        
+         
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();           
+        int T = Integer.parseInt(sc.nextLine());
+         
+         
         for (int t = 1; t <= T; t++)
         { 
-        	sc.nextInt();
-        	int score[] = new int[101];
-        	for(int i=0; i<1000; i++)
-        	{
-        		score[sc.nextInt()]++;
-        	}
-        	
-        	int mode = 0; //최빈값
-        	int answer = 0; //최빈값인 점수 
-        	
-        	for(int i=100; i>0; i--)
-        	{
-        		if(score[i]>mode) {
-        			mode = score[i]; //빈도수 저장
-        			answer= i; //최빈값
-        		}
-        	}
-        	
-        	System.out.printf("#%d %d\n",t,answer);
-        	
+            int score[] = new int[1000];
+            int answer = 0;
+            
+            sc.nextInt();
+            HashMap<Integer,Integer> map = new HashMap<>();
+            for(int i=0; i<1000; i++)
+            {
+                score[i] = sc.nextInt();
+                 
+                map.put(score[i],map.getOrDefault(score[i],0)+1);
+            }
+             
+            List<Integer> keySet = new ArrayList<>(map.keySet());
+             
+            //value를 기준으로 내림차순 정렬
+            List<Map.Entry<Integer,Integer>>list = new ArrayList<>(map.entrySet());
+            list.sort((e1,e2)->e2.getValue().compareTo(e1.getValue()));
+             
+             
+            //최빈값이 중복된다면
+            if(list.size()>1 && list.get(0).getValue().equals(list.get(1).getValue()))
+            {
+                if(list.get(0).getKey()>list.get(1).getKey())
+                {
+                    answer = list.get(0).getKey();
+                }
+                else
+                {
+                    answer = list.get(1).getKey();
+                }
+            }
+            else
+            {
+                answer = list.get(0).getKey();
+            }
+             
+            System.out.printf("#%d %d\n",t,answer);
+             
         }
     }
 }
